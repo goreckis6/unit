6e0fc5c3256cd79cc5a6f912216b47dbbdbe9cc4
goreckis6 <116648161+goreckis6@users.noreply.github.com>
2026-01-21 20:36:28 +0100

    FIX Code(31): Add manifest: qwikCityPlan to renderToStream - SSR MUST have manifest from @qwik-city-plan

--- src-qwik/entry.ssr.tsx
+++ src-qwik/entry.ssr.tsx
@@ -4,6 +4,7 @@ import Root from './root';
 
 export default function (opts: RenderOptions) {
   return renderToStream(<Root />, {
+    manifest: qwikCityPlan, // ⭐ KLUCZOWE - SSR musi mieć manifest z @qwik-city-plan
     ...opts,
     containerAttributes: {
       lang: 'en',

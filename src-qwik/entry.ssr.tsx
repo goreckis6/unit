06c19163d001201d9fb14fc1b53e64c5a9439075
goreckis6 <116648161+goreckis6@users.noreply.github.com>
2026-01-21 13:43:27 +0100

    Fix entry.ssr.tsx: Remove deprecated manifest import (Qwik API change)

--- src-qwik/entry.ssr.tsx
+++ src-qwik/entry.ssr.tsx
@@ -1,10 +1,8 @@
 import { renderToStream, type RenderOptions } from '@builder.io/qwik/server';
-import { manifest } from '@qwik-city-plan';
 import Root from './root';
 
 export default function (opts: RenderOptions) {
   return renderToStream(<Root />, {
-    manifest,
     ...opts,
     containerAttributes: {
       lang: 'en',

8d7c47a0cdce6aa3ec0f7acac2f9c3e2875f5040
goreckis6 <116648161+goreckis6@users.noreply.github.com>
2026-01-21 19:47:12 +0100

    Add favicon.svg and fix 404 errors for static assets

--- src-qwik/root.tsx
+++ src-qwik/root.tsx
@@ -7,6 +7,7 @@ export default component$(() => {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
